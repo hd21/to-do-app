d2ef93d43c4a632e34742fc127c5447530917b68
Hazel Doctor <hazel@dentalpost.net>
2018-08-24 21:38:38 -0400

    Add styles

--- app.js
+++ app.js
@@ -7,18 +7,38 @@ function onReady() {
     event.preventDefault();
 
     let title = newToDoText.value;
+
     let newLi = document.createElement('li');
+    newLi.className = 'mdl-list__item';
+
+    let primarySpan = document.createElement('span');
+    primarySpan.className = 'mdl-checkbox__label custom';
+    primarySpan.textContent = title;
+
+    let labelList = document.createElement('label');
+    labelList.className = 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect';
+    labelList.setAttribute('for', 'checkbox1');
     let checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
+    checkbox.className = 'mdl-checkbox__input';
+    checkbox.setAttribute('id', 'checkbox1');
 
-    newLi.textContent = title;
-    newLi.appendChild(checkbox);
-    newLi.appendChild(deleteButton);
+    let span = document.createElement('span');
+    span.className = 'mdl-list__item-secondary-action';
+    let deleteItem = document.createElement('i');
+    deleteItem.className = 'fas fa-trash';
+
+    newLi.appendChild(labelList);
+    newLi.appendChild(span);
+
+    labelList.appendChild(checkbox);
+    labelList.appendChild(primarySpan);
+    componentHandler.upgradeElement(labelList);
+
+    span.appendChild(deleteItem);
 
     toDoList.appendChild(newLi);
-    deleteButton.onclick = function() {
+    deleteItem.onclick = function() {
       toDoList.removeChild(newLi);
     };
 
