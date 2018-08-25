f5064821f9fe364970db23b29d0e0b3a0219bc1b
Hazel Doctor <hazel@dentalpost.net>
2018-08-24 21:53:35 -0400

    Refactor app.js

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
 function onReady() {
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) {
+      return;
+    }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+    });
+
+    newToDoText.value = '';
+
+    renderTheUI();
+  }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-
-    let title = newToDoText.value;
-
-    let newLi = document.createElement('li');
-    newLi.className = 'mdl-list__item';
-
-    let primarySpan = document.createElement('span');
-    primarySpan.className = 'mdl-checkbox__label custom';
-    primarySpan.textContent = title;
-
-    let labelList = document.createElement('label');
-    labelList.className = 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect';
-    labelList.setAttribute('for', 'checkbox1');
-    let checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = 'mdl-checkbox__input';
-    checkbox.setAttribute('id', 'checkbox1');
-
-    let span = document.createElement('span');
-    span.className = 'mdl-list__item-secondary-action';
-    let deleteItem = document.createElement('i');
-    deleteItem.className = 'fas fa-trash';
-
-    newLi.appendChild(labelList);
-    newLi.appendChild(span);
-
-    labelList.appendChild(checkbox);
-    labelList.appendChild(primarySpan);
-    componentHandler.upgradeElement(labelList);
-
-    span.appendChild(deleteItem);
-
-    toDoList.appendChild(newLi);
-    deleteItem.onclick = function() {
-      toDoList.removeChild(newLi);
-    };
-
-    newToDoText.value = '';
+    createNewToDo();
   });
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
 }
 
 window.onload = function() {
